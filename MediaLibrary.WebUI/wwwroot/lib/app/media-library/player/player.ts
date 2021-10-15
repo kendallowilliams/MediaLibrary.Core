e039b2556577373b311db9ad36a04672b8941ddc
Kendall Williams <kendallowilliams@outlook.com>
2021-10-15 19:24:02 -0400

    fixed player skip back issue

--- MediaLibrary.WebUI/wwwroot/lib/app/media-library/player/player.ts
+++ MediaLibrary.WebUI/wwwroot/lib/app/media-library/player/player.ts
@@ -491,7 +491,7 @@ export default class Player extends BaseClass implements IView {
     private skipBackward(): void {
         const player: HTMLMediaElement = this.getPlayer(),
             currentTime: number = player.currentTime,
-            updatedTime: number = currentTime - this.playerConfiguration.properties.SkipForwardSeconds;
+            updatedTime: number = currentTime - this.playerConfiguration.properties.SkipBackwardSeconds;
 
         if (updatedTime < 0) /*then*/ player.currentTime = 0;
         else /*then*/ player.currentTime = updatedTime;
