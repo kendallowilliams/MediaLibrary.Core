72406ca947b194206417d31734822a0ae735d5a9
Kendall Williams <kendallowilliams@outlook.com>
2022-01-18 15:21:20 -0500

    updated to fix issue with application not closing

--- MediaLibrary.Console/HostedServices/AppHostedService.cs
+++ MediaLibrary.Console/HostedServices/AppHostedService.cs
@@ -13,15 +13,18 @@ namespace MediaLibrary.Console.HostedServices
     public class AppHostedService : IHostedService
     {
         private readonly IProcessorService processorService;
+        private readonly IHostApplicationLifetime hostApplicationLifetime;
 
-        public AppHostedService(IProcessorService processorService)
+        public AppHostedService(IProcessorService processorService, IHostApplicationLifetime hostApplicationLifetime)
         {
             this.processorService = processorService;
+            this.hostApplicationLifetime = hostApplicationLifetime;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             await Task.WhenAll(processorService.RefreshMusic(), processorService.RefreshPodcasts(), processorService.PerformCleanup());
+            hostApplicationLifetime.StopApplication();
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
