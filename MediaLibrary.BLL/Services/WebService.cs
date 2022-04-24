84b7b7f518a35ae0f9a85227c87a5111659a9259
Kendall Williams <kendallowilliams@outlook.com>
2022-04-24 17:11:54 -0400

    webservice refactor

--- MediaLibrary.BLL/Services/WebService.cs
+++ MediaLibrary.BLL/Services/WebService.cs
@@ -1,82 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
-using System.Web;
 using MediaLibrary.BLL.Services.Interfaces;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using System.Text;
 using System.Threading;
+using System.IO;
 
 namespace MediaLibrary.BLL.Services
 {
     public class WebService : IWebService
     {
-        public WebService()
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public WebService(IHttpClientFactory httpClientFactory)
         {
+            this.httpClientFactory = httpClientFactory;
         }
 
-        public async Task<byte[]> DownloadData(string address, CancellationToken token = default, Action<int, long, long> progressChanged = null)
+        public async Task<byte[]> DownloadData(string address, CancellationToken token = default)
         {
-            TaskCompletionSource<byte[]> tcs = new TaskCompletionSource<byte[]>(token);
+            var result = default(byte[]);
 
-            using (WebClient client = new WebClient())
+            using (var client = httpClientFactory.CreateClient())
             {
-                try
-                {
-                    Uri uri = new Uri(address);
+                Uri uri = new Uri(address);
 
-                    client.DownloadProgressChanged += (sender, args) =>
-                    {
-                        progressChanged?.Invoke(args.ProgressPercentage, args.BytesReceived, args.TotalBytesToReceive);
-                    };
-                    client.DownloadDataCompleted += (sender, args) =>
-                    {
-                        if (args.Error == null) { tcs.SetResult(args.Result); }
-                        else { tcs.SetException(args.Error); }
-                    };
-                    client.DownloadDataAsync(uri);
-                }
-                catch (Exception ex)
+                result = await client.GetByteArrayAsync(uri);
+            }
+
+            return result;
+        }
+
+        public async Task<bool> DownloadFile(string address, string filename, CancellationToken token = default)
+        {
+            bool result = false;
+
+            using (var client = httpClientFactory.CreateClient())
+            {
+                Uri uri = new Uri(address);
+
+                using (var stream = await client.GetStreamAsync(uri))
                 {
-                    tcs.SetException(ex);
+                    using (var fileStream = File.OpenWrite(filename))
+                    {
+                        await stream.CopyToAsync(fileStream);
+                    }
                 }
             }
 
-            return await tcs.Task;
-        }
-
-        public async Task<bool> DownloadFile(string address, string filename, CancellationToken token = default, Action<int, long, long> progressChanged = null)
-        {
-            TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>(token);
-
-            using (WebClient client = new WebClient())
-            {
-                try
-                {
-                    Uri uri = new Uri(address);
-
-                    client.DownloadProgressChanged += (sender, args) =>
-                    {
-                        progressChanged?.Invoke(args.ProgressPercentage, args.BytesReceived, args.TotalBytesToReceive);
-                    };
-                    client.DownloadFileCompleted += (sender, args) =>
-                    {
-                        if (args.Error == null) { tcs.SetResult(true); }
-                        else { tcs.SetException(args.Error); }
-                    };
-                    client.DownloadFileAsync(uri, filename);
-                }
-                catch (Exception ex)
-                {
-                    tcs.SetException(ex);
-                }
-            }
-
-            return await tcs.Task;
+            return result;
         }
 
         public async Task<T> Get<T>(Uri baseUri, string relativePath, string username, string password)
@@ -85,15 +59,12 @@ namespace MediaLibrary.BLL.Services
             HttpResponseMessage response = default;
             string credentials = $"{username}:{password}",
                    authorization = Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials));
-            HttpClientHandler handler = new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate };
-
-            using (var client = new HttpClient(handler))
+            
+            using (var client = httpClientFactory.CreateClient("DecompressionClient"))
             {
                 client.BaseAddress = baseUri;
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authorization);
-                client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
-                client.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
                 response = await client.GetAsync(relativePath);
 
                 if (response.IsSuccessStatusCode)
@@ -111,7 +82,7 @@ namespace MediaLibrary.BLL.Services
             string credentials = $"{username}:{password}",
                    authorization = Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials));
 
-            using (var client = new HttpClient())
+            using (var client = httpClientFactory.CreateClient())
             {
                 client.BaseAddress = baseUri;
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authorization);
@@ -127,7 +98,7 @@ namespace MediaLibrary.BLL.Services
             string credentials = $"{username}:{password}",
                    authorization = Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials));
 
-            using (var client = new HttpClient())
+            using (var client = httpClientFactory.CreateClient())
             {
                 client.BaseAddress = baseUri;
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
